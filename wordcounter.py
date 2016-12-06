43ba3678142643ebf1b5aa8c97233e818c094772
Charis <charisbuxton@gmail.com>
2016-12-06 12:20:14 +0000

    Create wordcount
    We're counting their words

---
+++ wordcounter.py
@@ -0,0 +1,17 @@
+#We received an email from earth, we want to count the number of words in it
+#Yes, we're weird like that
+
+email = open("textfile goes here.txt","r+")
+
+#Create a dictionary to store every word
+wordcount = {}
+
+#Loop through the file and count every word
+
+for word in email.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+
+print(wordcount)
