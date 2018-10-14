67c4cfcc56feb1aeb438c7402b58838b0cc105ca
pkopoku <pkopoku42@gmail.com>
2018-10-14 18:52:31 +0100

    syntax correction and todo point

--- basic_greedy.py
+++ basic_greedy.py
@@ -1,16 +1,17 @@
 #A basic greedy alg
 
+#todo: establish generalized problem
 A={1,2,3,4,5}
 T=8
 time_used=0
 num_of_things_done=[]
 
 for i in A:
-if time_used>=T:
-break
-else:
-num_of_things_done.append(i)
-time_used+=i
+    if time_used >= T:
+        break
+    else:
+        num_of_things_done.append(i)
+        time_used+=i
 
 print(num_of_things_done)
 print(time_used)
